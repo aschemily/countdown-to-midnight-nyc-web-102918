6a81423b4fdabd18446c10c3a6b1540c9f1d048b
Emily Asch <e.asch@me.com>
2018-10-09 23:53:22 +0000

    automatically backed up by learn

--- countdown.rb
+++ countdown.rb
@@ -1,10 +1,9 @@
 #write your code here
 
-def countdown
-  x = 1 
-  while x < 10 
-  puts "#{number} SECOND(S)!"
-  x -= 1 
+def countdown(number)
+ while number > 0 
+ end
+ 
 end
 return "HAPPY NEW YEAR!"
 end
